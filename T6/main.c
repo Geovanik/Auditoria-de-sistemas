2660bc91a5a17ec7f990f54109a41d317b322be1
GeovaniK <geovani2k@yahoo.com.br>
2015-11-08 01:17:26 -0200

    Alteração no modo de leitura da entrada

--- T6/main.c
+++ T6/main.c
@@ -2,17 +2,47 @@
  a entrada é um vetor de 10 posições, cada posição com 10⁸ valores
  int de 32 bits
  
- * 
+ * //entrada em arquivo
+ * //vetor de resultado de 1024 bits, 37 posicoes em que cada uma suporta 8 digitos
+ * int 10 digitos
  * */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAX 1123456789 
 
+void multiplicacao(int entrada1[], int entrada2[], int resultado[]){
+	int cont=9;
+	while(cont>=0){
+		resultado[cont]=entrada1[cont]*entrada2[cont];
+		cont--;
+	}
+}
+
+void soma(int entrada1[], int entrada2[], int resultado[]){
+	int cont=9;
+	while(cont>=0){
+		resultado[cont]=entrada1[cont]+entrada2[cont];
+		cont--;
+	}
+}
+
+void exponenciacao(){
+	
+	
+	
+}
+
+void inverso_modular(){
+	
+	
+	
+}
 void inicializa_vetores(int tamanho, int vetor[]){
 	int a=0;
 	while(a<tamanho)//inicializa vetror de swap2
 		vetor[a++]=0;
-	}
+}
 
 void uniao_valores(int swap2[],int entrada[],int posicao){
 	int valor=0;
@@ -25,23 +55,19 @@ void uniao_valores(int swap2[],int entrada[],int posicao){
 	valor=(swap2[2]*100000)+valor;
 	valor=(swap2[1]*1000000)+valor;
 	valor=(swap2[0]*10000000)+valor;
-	printf("valor de 8 bits %d",valor);
 	
 	entrada[posicao]=valor;
-	printf("entrada  %d",entrada[posicao]);
 	
 	inicializa_vetores(8,swap2);
 }
 
 void quebra_valores(long long int swap[],int entrada[],int t){//quebra os valores da entrada colocando eles na entrada
-	int swap2[8],a=0,cont=7,posicao=9;
+	int swap2[8],cont=7,posicao=9;
 	long long int valor=0;
 	
-	while(a<8)//inicializa vetror de swap2
-		swap2[a++]=0;
+	inicializa_vetores(8,swap2);
 		
 	valor=swap[--t];
-	printf("\n%lld",valor);
 	while (t>-1){//controla a quantidade de numeros dentro do vetor de swap
 
 		swap2[cont--]=(valor%10);//colocando os digitos no vetor de swap/
@@ -49,54 +75,138 @@ void quebra_valores(long long int swap[],int entrada[],int t){//quebra os valore
 			uniao_valores(swap2,entrada,posicao);
 			posicao--;
 			cont=7;
-		}
-			
-		//printf("\nvalor %lld ",valor);
+		}	
 		valor/=10;
 		if(valor==0)
 			valor=swap[--t];
-		
-		
 	}
-	//printf("\nswap 2 %d\n",swap2[0]);
 	if(swap2[7]!=0)//para valores menores que 8 bits, valores mais significativos
 		uniao_valores(swap2,entrada,posicao);
-	
-	cont=0;
-	while(cont<8){
-		printf("\n%d ",cont);
-		printf("swap %d\n",swap2[cont++]);
-	}	
 }
-
-int main(){
-	int entrada[10],controle=1,t=0;
-	long long int valor=0,swap[50];
-	//inicializando vetor swap
+int main(int argc,char**argv){
+	int entrada1[20],entrada2[20],resultado[39],cont=0,contador=8,posicao=10,cont_entrada=19,flag=1;
+	//resultado em que cada posicao tem ate 8 digitos
+	//os valores de entrada podem ser do max do tamanho 8*20
+	//flag 1 indica entrada2 0 a entrada1
+	
+	char lendo='a',swap[10];
+	FILE *pt_file=NULL;
+	
+	inicializa_vetores(20,entrada1);
+	inicializa_vetores(20,entrada2);
+	inicializa_vetores(39,resultado);
+	
+	pt_file = fopen(argv[1], "r");
+	if(!pt_file){
+		printf("Impossivel abrir arquivo passado\n");
+		return 0;
+	}
+	fseek(pt_file, -2, SEEK_END);//posiciona o pont no final do arq no ultimo digito do numero
+	cont=-2;
+	//recebendo a entrada preenchendo o vetor de swap
+	//LER DE 8 EM 8 E COLOCAR EM UMA POSICAO DA ENTRADA
+	swap[contador--]='\0';//delimitando swap para 8 digitos
+	while(posicao>0){													//le o arquivo até encontrar o : e guarda a posição
+		
+		if(lendo=='\n'){//muda o  vetor de entradas, reinicializa variaveis
+			if(contador!=-1)
+				printf("diferente");
+			flag=0;
+			cont_entrada=19;
+			contador=8;
+			swap[contador--]='\0';//delimitando swap para 8 digitos
+		}
+			
+		lendo = fgetc(pt_file);
+		cont--;
+		fseek(pt_file, cont, SEEK_END);
+		posicao=ftell(pt_file);
+		
+		swap[contador]=lendo;
+		contador--;
+		
+		if(contador==-1){//8 dígitos foram lidos
+			if(flag)
+				entrada2[cont_entrada]=atoi(swap);
+			else
+				entrada1[cont_entrada]=atoi(swap);
+			
+			//printf("entra %d\n",entrada1[cont_entrada]);
+			cont_entrada--;
+			
+			contador=8;
+			swap[contador--]='\0';
+		
+		}
+	}
+	/*int h=0;
+	while(h<17){
+		printf("%d ",h);
+		printf("entrada1 %d\n",entrada1[h++]);
+	}*/
+	/*while(controle==1){
+		scanf("%lld",&valor);
+		swap[t++]=valor;
+		
+		printf("Deseja continuar digitando um mesmo numero?\n 1 - sim\n 0 - nao\n");
+		scanf("%d",&controle);
+	}*/
+	/*
+	quebra_valores(swap,entrada1,t);//quebrando os digitos e colocando na entrada
 	
 	while(t<50){
 		swap[t++]=0;
 	}
+	controle=1;
 	t=0;
-	while(t<10){
-		entrada[t++]=0;
-	}
-	t=0;
-	//recebendo a entrada preenchendo o vetor de swap
-	printf("Digite a entrada, 18 digitos por vez\n");
+	
+	printf("Digite a entrada para o segundo numero, 18 digitos por vez\n");
 	while(controle==1){
 		scanf("%lld",&valor);
-		printf("%lld",valor);
 		swap[t++]=valor;
 		
 		printf("Deseja continuar digitando um mesmo numero?\n 1 - sim\n 0 - nao\n");
 		scanf("%d",&controle);
 	}
-	quebra_valores(swap,entrada,t);//quebrando os digitos e colocando na entrada
+	quebra_valores(swap,entrada2,t);//quebrando os digitos e colocando na entrada
+	
+	printf("\nqual e a operacao? 1 - soma\t 2 - exponenciacao\t 3 - inverso modular\n");
+	scanf("%d",&operacao);
+	if(operacao==1)
+		soma(entrada1,entrada2,resultado);
+	else if(operacao==2)
+		exponenciacao();
+	
+	else 
+		inverso_modular();
+	
 	int h=0;
 	while(h<10){
 		printf("%d ",h);
-		printf("entrada %d\n",entrada[h++]);
+		printf("entrada1 %d\n",entrada1[h++]);
+	}
+	
+	
+	h=0;
+	while(h<37){
+		printf("%d ",h);
+		printf("resultado %d\n",resultado[h++]);
+	}
+	* 
+	* while(a>=0)
+				printf("%c",swap[a--]);
+	* */
+	
+	int h=0;
+	while(h<20){
+		printf("\n%d ",h);
+		printf("entrada1 %d\n",entrada1[h++]);
+	}
+	
+	h=0;
+	while(h<20){
+		printf("%d ",h);
+		printf("entrada2 %d\n",entrada2[h++]);
 	}
 	
 	return 0;
